11f278ce1747ecef1a108da2f0d369bb4e7b581e
Jamie <Jamiekaram16@gmail.com>
2020-04-21 17:25:42 -0400

    add scss

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,11 +3,13 @@ import React from 'react'
 import Header from './header'
 import Footer from './footer'
 
+import '../styles/index.scss'
+
 const Layout = (props) => {
     return (
         <div>
             <Header/>
-            {props.children}
+                {props.children}
             <Footer />
         </div>
     )
